472884df8fbd86fbf022c55afa0f92329186de0a
Nicolas <Vargas>
2022-05-06 22:42:39 -0500

    se añadio el routing a registrar estudiantes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IniciarSesionComponent } from './Components/iniciar-sesion/iniciar-sesion.component';
 import { InicioComponent } from './Components/inicio/inicio.component';
+import { RegistrarEstudianteComponent } from './Components/registrar-estudiante/registrar-estudiante.component';
 import { RegistrarProfesorComponent } from './Components/registrar-profesor/registrar-profesor.component';
 
 const routes: Routes = [
@@ -10,7 +11,8 @@ const routes: Routes = [
   },
   {path: './home',component: InicioComponent},
   {path: './iniciar',component: IniciarSesionComponent},
-  {path: './registrar-profesor',component:RegistrarProfesorComponent}
+  {path: './registrar-profesor',component:RegistrarProfesorComponent},
+  {path: './registrar-estudiante',component:RegistrarEstudianteComponent}
 ];
 
 @NgModule({
